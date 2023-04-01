22ae25e20c8bbc070be35058e82ef93df26867f7
Mashoorkhan <129406045+Mashoorkhan@users.noreply.github.com>
2023-04-01 23:21:14 +0530

    Add files via upload

---
+++ years lesft py.py
@@ -0,0 +1,9 @@
+#imagine we live 90 years.......
+
+age = int(input("enter your age"))
+
+years_left = 90 - age
+days_left  = years_left * 365
+months_left = years_left * 12
+weeks_left  = years_left * 52
+print(f"you have {days_left} days, {months_left} months, and {weeks_left} weeks left .")
